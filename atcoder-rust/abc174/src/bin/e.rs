f25b100bb42a80237ecc5ab4c54fee929fe5dc39
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-02-22 18:19:12 +0900

    update

--- atcoder-rust/abc174/src/bin/e.rs
+++ atcoder-rust/abc174/src/bin/e.rs
@@ -1,6 +1,27 @@
-// TODO
 use proconio::input;
 
+fn run(_n: usize, k: usize, aa: Vec<usize>) -> usize {
+    // length
+    let mut ng = 0;
+    let mut ok = *aa.iter().max().unwrap();
+
+    while ng < ok - 1 {
+        let md = (ng + ok) / 2;
+        if aa.iter().map(|&a| (a - 1) / md).sum::<usize>() <= k {
+            ok = md;
+        } else {
+            ng = md;
+        }
+    }
+    return ok;
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        aa: [usize; n]
+    }
+
+    println!("{}", run(n, k, aa));
 }
--- atcoder-rust/abc174/src/bin/f.rs
+++ atcoder-rust/abc174/src/bin/f.rs
@@ -1,6 +1,54 @@
-// TODO
-use proconio::input;
+// TODO (still TLE)
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        cs: [usize; n],
+        lr: [[usize; 2]; q]
+    }
+
+    let mut cn = Vec::with_capacity(n);
+    unsafe {
+        cn.set_len(n);
+    }
+    for i in 0..n {
+        cn[i] = 0;
+    }
+    for c in cs.clone() {
+        cn[c - 1] += 1
+    }
+
+    let mut block: Vec<(usize, usize)> = Vec::new();
+
+    for i in 0..n {
+        if cn[cs[i] - 1] < 2 {
+            continue;
+        }
+        cn[cs[i] - 1] -= 1;
+        for j in (i + 1)..n {
+            if cs[i] == cs[j] {
+                block.push((i + 1, j + 1));
+                break;
+            }
+        }
+    }
+
+    for i in 0..q {
+        let l = lr[i][0];
+        let r = lr[i][1];
+
+        let mut count = 0;
+        for &(bs, be) in block.iter() {
+            if be <= r && l <= bs {
+                count += 1;
+            }
+            if r <= bs {
+                break;
+            }
+        }
+        println!("{}", r - l + 1 - count);
+    }
 }
