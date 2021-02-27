bc640551e98365f0023d0ac4fb9e0278bc2b6221
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-02-27 17:50:44 +0900

    Add d, e

--- atcoder-rust/dp/src/bin/d.rs
+++ atcoder-rust/dp/src/bin/d.rs
@@ -1,8 +1,33 @@
-// TODO
+use std::cmp::max;
 
 use proconio::{fastout, input};
 
+fn run(n: usize, w: usize, wv: Vec<Vec<usize>>) -> usize {
+    let mut a1 = [0; 100001];
+    let mut a2 = [0; 100001];
+    for i in 0..n {
+        let iw = wv[i][0];
+        let iv = wv[i][1];
+        for j in 0..(w + 1) {
+            if j < iw {
+                a2[j] = a1[j];
+            } else {
+                a2[j] = max(a1[j], a1[j - iw] + iv);
+            }
+        }
+        a1 = a2;
+    }
+
+    return a1[w];
+}
+
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        w: usize,
+        wv: [[usize; 2]; n]
+    }
+
+    println!("{}", run(n, w, wv));
 }
--- atcoder-rust/dp/src/bin/e.rs
+++ atcoder-rust/dp/src/bin/e.rs
@@ -1,8 +1,43 @@
-// TODO
+use std::cmp::min;
 
 use proconio::{fastout, input};
 
+fn run(n: usize, w: usize, wv: Vec<Vec<usize>>) -> usize {
+    let sum_v: usize = wv.iter().map(|v| v[1]).sum();
+    let mut a1 = [1_000_000_000_000; 100001];
+    let mut a2 = [0; 100001];
+
+    a1[0] = 0;
+    for i in 0..n {
+        let iw = wv[i][0];
+        let iv = wv[i][1];
+        for j in 0..(sum_v + 1) {
+            if j < iv {
+                a2[j] = a1[j];
+            } else {
+                a2[j] = min(a1[j], a1[j - iv] + iw);
+            }
+        }
+        a1 = a2;
+    }
+
+    let mut v = 0;
+    for j in 0..(sum_v + 1) {
+        if a1[j] <= w && v < j {
+            v = j
+        }
+    }
+
+    return v;
+}
+
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        w: usize,
+        wv: [[usize; 2]; n]
+    }
+
+    println!("{}", run(n, w, wv));
 }
