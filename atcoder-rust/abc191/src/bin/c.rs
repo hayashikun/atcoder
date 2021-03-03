eea7f7b34d3694d12617986df1906f159601f40f
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-03 11:43:23 +0900

    Add c, d

--- atcoder-rust/abc191/src/bin/c.rs
+++ atcoder-rust/abc191/src/bin/c.rs
@@ -1,8 +1,26 @@
-// TODO
-
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        h: usize,
+        w: usize,
+        gr: [String; h]
+    };
+
+    let gr: Vec<Vec<usize>> = gr
+        .iter()
+        .map(|s| s.chars().map(|c| if c == '.' { 0 } else { 1 }).collect())
+        .collect();
+
+    let mut corner = 0;
+    for y in 0..(h - 1) {
+        for x in 0..(w - 1) {
+            if (gr[y][x] + gr[y + 1][x] + gr[y][x + 1] + gr[y + 1][x + 1]) % 2 == 1 {
+                corner += 1;
+            }
+        }
+    }
+
+    println!("{}", corner);
 }
--- atcoder-rust/abc191/src/bin/d.rs
+++ atcoder-rust/abc191/src/bin/d.rs
@@ -1,8 +1,33 @@
-// TODO
-
+use num::integer::{div_ceil, sqrt};
+use num_integer::div_floor;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        x: f64,
+        y: f64,
+        r: f64
+    };
+
+    let x = (10000.0 * x).round() as isize;
+    let y = (10000.0 * y).round() as isize;
+    let r = (10000.0 * r).round() as isize;
+
+    let mut sum = 0;
+    for cx in (x - r) / 10000..=(x + r) / 10000 {
+        let c2 = r.pow(2);
+        let a2 = (cx * 10000 - x).pow(2);
+        if a2 > c2 {
+            continue;
+        }
+        let b = sqrt(c2 - a2);
+
+        let y1 = div_floor(y + b, 10000);
+        let y2 = div_ceil(y - b, 10000);
+
+        sum += y1 - y2 + 1;
+    }
+
+    println!("{}", sum)
 }
