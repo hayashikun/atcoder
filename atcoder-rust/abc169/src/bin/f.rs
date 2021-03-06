96a5c9e426f1909a904516ab4d9febd99c89a5b6
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-06 17:36:35 +0900

    Add f

--- atcoder-rust/abc169/src/bin/f.rs
+++ atcoder-rust/abc169/src/bin/f.rs
@@ -1,8 +1,26 @@
-// TODO
-
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    let m = 998244353;
+    input! {
+        n: usize,
+        s: usize,
+        aa: [usize; n]
+    }
+
+    let mut dp = vec![vec![0i128; s + 1]; n + 1];
+    dp[0][0] = 1;
+
+    for i in 0..n {
+        for j in 0..(s + 1) {
+            dp[i + 1][j] += dp[i][j] * 2 % m;
+            if j >= aa[i] {
+                dp[i + 1][j] += dp[i][j - aa[i]];
+            }
+            dp[i + 1][j] %= m;
+        }
+    }
+
+    println!("{}", dp[n][s]);
 }
