b94eec0c3686b63eac31ccbb994aa15838716260
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-07 14:36:30 +0900

    solve f

--- atcoder-rust/abc181/src/bin/f.rs
+++ atcoder-rust/abc181/src/bin/f.rs
@@ -5,7 +5,8 @@ use std::cmp::min;
 
 use itertools::Itertools;
 use libm::sqrt;
-use num_traits::Pow;
+use num::traits::Pow;
+use petgraph::unionfind::UnionFind;
 use proconio::{fastout, input};
 
 #[fastout]
@@ -15,24 +16,36 @@ fn main() {
         xy: [(isize, isize); n]
     }
 
-    let xy = xy
-        .into_iter()
-        .sorted_by_key(|p| p.1)
-        .map(|p| (p.0 as f64, p.1 as f64))
-        .collect_vec();
-    let mut min_dis = 200.0;
+    println!("{}", run(n, xy));
+}
+
+fn run(n: usize, xy: Vec<(isize, isize)>) -> f64 {
+    // length, xy from, xy to
+    let mut edges: Vec<(f64, usize, usize)> = Vec::new();
 
     for i in 0..n {
+        let xi = xy[i].0 as f64;
+        let yi = xy[i].1 as f64;
+        edges.push((100.0 - yi, i, n));
+        edges.push((yi + 100.0, i, n + 1));
         for j in (i + 1)..n {
-            if xy[j].1 - xy[i].1 > min_dis {
-                break;
-            }
-            let dis = sqrt((xy[j].0 - xy[i].0).pow(2) + (xy[j].1 - xy[i].1).pow(2));
-            if dis < min_dis {
-                min_dis = dis;
-            }
+            let xj = xy[j].0 as f64;
+            let yj = xy[j].1 as f64;
+            let r = sqrt((xi - xj).pow(2) + (yi - yj).pow(2));
+            edges.push((r, i, j));
+        }
+    }
+    edges.sort_by(|&a, &b| a.0.partial_cmp(&b.0).unwrap());
+
+    let mut uf = UnionFind::new(n + 2);
+
+    for e in edges {
+        uf.union(e.1, e.2);
+
+        if uf.equiv(n, n + 1) {
+            return e.0 / 2.0;
         }
     }
 
-    println!("{}", min_dis);
+    return 0.0;
 }
