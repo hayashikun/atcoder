2d1fe111e9b5f7934a41710ac63ab125b872170c
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-07 22:44:26 +0900

    solve f

--- atcoder-rust/abc191/src/bin/f.rs
+++ atcoder-rust/abc191/src/bin/f.rs
@@ -1,8 +1,46 @@
-// TODO
+use std::collections::HashMap;
 
+use num_integer::gcd;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize,
+        mut aa: [usize; n]
+    };
+
+    aa.sort();
+    aa.dedup();
+
+    let min = aa[0];
+
+    let mut map = HashMap::new();
+
+    for a in aa {
+        for i in 1.. {
+            if i * i > a {
+                break;
+            }
+            if a % i != 0 {
+                continue;
+            }
+            if i < min {
+                let p = map.entry(i).or_insert(a);
+                *p = gcd(*p, a);
+            }
+            if a / i < min {
+                let p = map.entry(a / i).or_insert(a);
+                *p = gcd(*p, a);
+            }
+        }
+    }
+
+    let mut ans = 1;
+    for (v, c) in map {
+        if v == c {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
