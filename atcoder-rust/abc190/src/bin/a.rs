a2a54db541690a5386851eff4cd3c9e50b01acf5
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-09 01:02:25 +0900

    Add abc190

---
+++ atcoder-rust/abc190/src/bin/a.rs
@@ -0,0 +1,28 @@
+// TODO
+#![allow(unused_imports)]
+
+use proconio::{fastout, input, marker::*};
+
+#[fastout]
+fn main() {
+    input!{
+        a: usize,
+        b: usize,
+        c: usize
+    };
+
+    let s = if a == b {
+        if c == 0 {
+            "Aoki"
+        } else {
+            "Takahashi"
+        }
+    } else {
+        if a > b {
+            "Takahashi"
+        } else {
+            "Aoki"
+        }
+    };
+    println!("{}", s);
+}
