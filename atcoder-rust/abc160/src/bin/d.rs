5e5d81fedbb573d9daeda825d6ff74de768c5041
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-17 13:29:29 +0900

    solve d

--- atcoder-rust/abc160/src/bin/d.rs
+++ atcoder-rust/abc160/src/bin/d.rs
@@ -1,9 +1,30 @@
-// TODO
 #![allow(unused_imports)]
 
+use nalgebra::min;
+use num::abs;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: isize,
+        x: Isize1,
+        y: Isize1,
+    };
+
+    let mut counts = vec![0; (n - 1) as usize];
+
+    for i in 0..(n - 1) {
+        for j in (i + 1)..n {
+            let mut d = j - i;
+            if x <= j && i <= y {
+                d = min(d, abs(i - x) + 1 + abs(j - y))
+            }
+            counts[(d - 1) as usize] += 1;
+        }
+    }
+
+    for c in counts {
+        println!("{}", c);
+    }
 }
