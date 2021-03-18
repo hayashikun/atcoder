ef579ed046cf8454c83072160cc44caac58e2d69
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-18 21:19:34 +0900

    solve d

--- atcoder-rust/abc178/src/bin/d.rs
+++ atcoder-rust/abc178/src/bin/d.rs
@@ -1,9 +1,28 @@
-// TODO
 #![allow(unused_imports)]
+
+use std::cmp::max;
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        s: usize
+    };
+
+    if s < 3 {
+        println!("{}", 0);
+        return;
+    }
+
+    let d = 10usize.pow(9) + 7;
+
+    let mut dp = vec![0; s + 1];
+    dp[0] = 1;
+
+    for i in 3..=s {
+        dp[i] = (dp[i - 1] + dp[i - 3]) % d;
+    }
+
+    println!("{}", dp[s]);
 }
