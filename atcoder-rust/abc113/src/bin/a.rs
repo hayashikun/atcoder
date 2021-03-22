12243afab81653874dd96302ae8ad90f8744d966
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-22 23:54:24 +0900

    ac some

--- atcoder-rust/abc113/src/bin/a.rs
+++ atcoder-rust/abc113/src/bin/a.rs
@@ -1,9 +1,13 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input!{
+        x: usize,
+        y: usize
+    };
+
+    println!("{}", x + y / 2);
 }
--- atcoder-rust/abc127/src/bin/a.rs
+++ atcoder-rust/abc127/src/bin/a.rs
@@ -1,9 +1,19 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        a: usize,
+        b: usize
+    };
+
+    if a >= 13 {
+        println!("{}", b);
+    } else if a >= 6 {
+        println!("{}", b / 2);
+    } else {
+        println!("0");
+    }
 }
