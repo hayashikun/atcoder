c86feb8c866486f8792103d99e79402812275df2
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-24 00:28:08 +0900

    solve

--- atcoder-rust/abc127/src/bin/b.rs
+++ atcoder-rust/abc127/src/bin/b.rs
@@ -1,9 +1,17 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input!{
+        r: usize,
+        d: usize,
+        mut x: usize
+    };
+
+    for _ in 0..10 {
+        x = r * x - d;
+        println!("{}", x);
+    }
 }
--- atcoder-rust/abc127/src/bin/c.rs
+++ atcoder-rust/abc127/src/bin/c.rs
@@ -1,9 +1,29 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize,
+        m: usize,
+        lr: [(Usize1, Usize1); m]
+    };
+
+    let mut li = 0;
+    let mut ri = n - 1;
+    for (l, r) in lr {
+        if li < l {
+            li = l;
+        }
+        if r < ri {
+            ri = r;
+        }
+    }
+
+    if li > ri {
+        println!("{}", 0);
+    } else {
+        println!("{}", ri - li + 1);
+    }
 }
