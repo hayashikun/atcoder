20496bae44f2ff3de01f103e17660cacbbc135be
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-23 23:40:36 +0900

    solve

--- atcoder-rust/abc094/src/bin/b.rs
+++ atcoder-rust/abc094/src/bin/b.rs
@@ -1,9 +1,26 @@
-// TODO
 #![allow(unused_imports)]
 
+use nalgebra::min;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        _n: usize,
+        m: usize,
+        x: usize,
+        aa: [usize; m]
+    };
+
+    let mut left = 0;
+    let mut right = 0;
+
+    for a in aa {
+        if a < x {
+            left += 1;
+        } else {
+            right += 1;
+        }
+    }
+    println!("{}", min(left, right));
 }
