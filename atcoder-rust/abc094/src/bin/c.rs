088c064df593badfc4a2fcb87ddf4c5cde973eca
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-24 00:12:20 +0900

    solve abc094-c

--- atcoder-rust/abc094/src/bin/c.rs
+++ atcoder-rust/abc094/src/bin/c.rs
@@ -1,9 +1,25 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
+use itertools::Itertools;
 
 #[fastout]
 fn main() {
-    input!{};
+    input!{
+        n: usize,
+        mut xx: [usize; n]
+    };
+
+    let mut sxx = xx.clone();
+    sxx.sort();
+    let m1 = sxx[n / 2];
+    let m2 = sxx[n / 2 - 1];
+
+    for x in xx {
+        if x > m2 {
+            println!("{}", m2);
+        } else {
+            println!("{}", m1);
+        }
+    }
 }
