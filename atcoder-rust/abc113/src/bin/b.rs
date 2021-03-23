82d401aac635f579121e8b10b7a4c425ec11187f
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-23 23:59:02 +0900

    solve abc113-b

--- atcoder-rust/abc113/src/bin/b.rs
+++ atcoder-rust/abc113/src/bin/b.rs
@@ -1,9 +1,26 @@
-// TODO
 #![allow(unused_imports)]
 
+use itertools::Itertools;
+use num::abs;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: isize,
+        t: isize,
+        a: isize,
+        hh: [isize; n]
+    };
+    let a = 1000 * a;
+    let t = 1000 * t;
+
+    let (i, _) = hh
+        .into_iter()
+        .map(|h| abs(t - 6 * h - a))
+        .enumerate()
+        .min_by_key(|&(_, d)| d)
+        .unwrap();
+
+    println!("{}", i + 1);
 }
