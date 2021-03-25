caeb1a034041d01c2a020657ddd1fa5e18278d8d
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-26 01:18:20 +0900

    solve

--- atcoder-rust/abc130/src/bin/a.rs
+++ atcoder-rust/abc130/src/bin/a.rs
@@ -1,9 +1,13 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        x: usize,
+        a: usize
+    };
+
+    println!("{}", if x < a { 0 } else { 10 });
 }
--- atcoder-rust/abc130/src/bin/b.rs
+++ atcoder-rust/abc130/src/bin/b.rs
@@ -1,9 +1,25 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input!{
+        n: usize,
+        x: usize,
+        ll: [usize; n]
+    };
+
+    let mut count = 1;
+    let mut current = 0;
+
+    for l in ll.into_iter() {
+        current += l;
+        if current <= x {
+            count += 1;
+        } else {
+            break
+        }
+    }
+    println!("{}", count);
 }
