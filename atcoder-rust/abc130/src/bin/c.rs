12be36ba2c79ea6c3f70eb60e5f34113b6a18988
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-27 00:24:19 +0900

    solve

--- atcoder-rust/abc130/src/bin/c.rs
+++ atcoder-rust/abc130/src/bin/c.rs
@@ -1,9 +1,19 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        w: usize,
+        h: usize,
+        x: usize,
+        y: usize,
+    };
+
+    println!(
+        "{} {}",
+        (w * h) as f64 / 2.0,
+        if w == x * 2 && h == y * 2 { 1 } else { 0 }
+    );
 }
