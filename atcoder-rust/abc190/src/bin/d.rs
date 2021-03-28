fe8d5950d4987268cc46da96b3e6a5d23e8b12cd
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-03-28 20:29:20 +0900

    solve

--- atcoder-rust/abc190/src/bin/d.rs
+++ atcoder-rust/abc190/src/bin/d.rs
@@ -1,9 +1,23 @@
-// TODO
 #![allow(unused_imports)]
 
+use num::integer::sqrt;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize
+    };
+
+    let tn = n * 2;
+    let mut ans = 0;
+    for i in 1..=sqrt(tn) {
+        if tn % i == 0 {
+            if (i + tn / i) % 2 == 1 {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans * 2);
 }
