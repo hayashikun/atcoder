b122b3a4fe3c4aa86f9254140f78218b3755d70b
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-01 23:10:46 +0900

    solve a

--- atcoder-rust/code-festival-2015-morning-middle/src/bin/a.rs
+++ atcoder-rust/code-festival-2015-morning-middle/src/bin/a.rs
@@ -1,9 +1,32 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize,
+        k: usize,
+        m: usize,
+        r: usize,
+        mut ss: [usize; n-1]
+    };
+
+    ss.sort();
+    ss.reverse();
+
+    let req_sum = k * r;
+    let cs = ss[0..(k-1)].iter().sum::<usize>();
+
+
+    if (k == n && cs >= req_sum) || (k != n && cs + ss[k - 1] >= req_sum) {
+        println!("{}", 0);
+        return;
+    }
+
+    if cs + m < req_sum {
+        println!("{}", -1);
+        return;
+    }
+    println!("{}", req_sum - cs);
 }
