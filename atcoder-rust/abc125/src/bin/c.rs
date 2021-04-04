4de200cee8d94005e449ed21d99f674cf6de2727
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-04 22:39:41 +0900

    solve

--- atcoder-rust/abc125/src/bin/c.rs
+++ atcoder-rust/abc125/src/bin/c.rs
@@ -1,9 +1,36 @@
-// TODO
 #![allow(unused_imports)]
 
+use std::cmp::max;
+
+use num_integer::gcd;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize,
+        aa: [usize; n]
+    };
+
+    let mut f = vec![0; n - 1];
+    f[0] = aa[0];
+    for i in 1..(n - 1) {
+        f[i] = gcd(f[i - 1], aa[i]);
+    }
+
+    let mut b = vec![0; n - 1];
+    b[n - 2] = aa[n - 1];
+    for i in 1..(n - 1) {
+        b[n - 2 - i] = gcd(b[n - 1 - i], aa[n - i - 1]);
+    }
+
+    let mut ans = max(f[n - 2], b[0]);
+    for i in 0..(n - 2) {
+        let a = gcd(f[i], b[i + 1]);
+        if a > ans {
+            ans = a;
+        }
+    }
+
+    println!("{}", ans);
 }
