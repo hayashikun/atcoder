4f0035b69fa5680ca6eaa9daeb05ffa815fe6df4
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-07 00:51:19 +0900

    solve b

--- atcoder-rust/abc111/src/bin/b.rs
+++ atcoder-rust/abc111/src/bin/b.rs
@@ -1,9 +1,22 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        mut n: usize
+    };
+
+    loop {
+        let d1 = n % 10;
+        let d2 = (n % 100) / 10;
+        let d3 = n / 100;
+
+        if (d1 == d2) && (d2 == d3) {
+            println!("{}", n);
+            break;
+        }
+        n += 1;
+    }
 }
