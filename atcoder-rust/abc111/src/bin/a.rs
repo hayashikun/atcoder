d4a87093473b997c9164622ce017249ab235696f
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-07 00:39:55 +0900

    solve

--- atcoder-rust/abc111/src/bin/a.rs
+++ atcoder-rust/abc111/src/bin/a.rs
@@ -1,9 +1,15 @@
-// TODO
 #![allow(unused_imports)]
 
+use itertools::Itertools;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: Chars
+    };
+
+    let ans = n.iter().map(|&c| if c == '9' { "1" } else { "9" }).join("");
+
+    println!("{}", ans);
 }
