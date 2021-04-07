5f18d74f545441968dbf162f1e7fd2d0103ec9dc
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-07 23:47:55 +0900

    solve a

--- atcoder-rust/nikkei2019-2-qual/src/bin/a.rs
+++ atcoder-rust/nikkei2019-2-qual/src/bin/a.rs
@@ -1,9 +1,16 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input!{
+        n: usize
+    };
+
+    if n % 2 == 0 {
+        println!("{}", n / 2 - 1);
+    } else {
+        println!("{}", n / 2)
+    }
 }
