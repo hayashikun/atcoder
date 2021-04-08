ac4a1367265c9de5226357d514a90cec9ee00b78
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-08 20:27:38 +0900

    solve d

--- atcoder-rust/abc141/src/bin/d.rs
+++ atcoder-rust/abc141/src/bin/d.rs
@@ -1,9 +1,26 @@
-// TODO
 #![allow(unused_imports)]
+
+use std::collections::BinaryHeap;
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize,
+        m: usize,
+        aa: [usize; n]
+    };
+
+    let mut heap = BinaryHeap::new();
+    for a in aa.into_iter() {
+        heap.push(a);
+    }
+
+    for _ in 0..m {
+        let a = heap.pop().unwrap();
+        heap.push(a / 2);
+    }
+
+    println!("{}", heap.into_iter().sum::<usize>());
 }
