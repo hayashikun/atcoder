bf27bd4c77168adcb7a05bf441ca876ede41e224
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-10 23:41:02 +0900

    solve b

--- atcoder-rust/abc026/src/bin/b.rs
+++ atcoder-rust/abc026/src/bin/b.rs
@@ -1,9 +1,23 @@
-// TODO
 #![allow(unused_imports)]
 
+use std::f64::consts::PI;
+
+use itertools::Itertools;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize,
+        rr: [i32; n]
+    };
+
+    let mut s = 0;
+    let mut pm = 1;
+    for r in rr.into_iter().sorted().rev() {
+        s += pm * r * r;
+        pm = -pm;
+    }
+
+    println!("{}", s as f64 * PI);
 }
