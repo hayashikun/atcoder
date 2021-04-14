f21c57052e02bf1a0ee31399275df41a9fc65489
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-15 00:48:16 +0900

    solve

--- atcoder-rust/abc178/src/bin/e.rs
+++ atcoder-rust/abc178/src/bin/e.rs
@@ -1,9 +1,25 @@
-// TODO
 #![allow(unused_imports)]
 
+use itertools::Itertools;
+use nalgebra::max;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        n: usize,
+        xy: [(isize, isize); n]
+    };
+
+    let mut fx = xy.iter().map(|(x, y)| x + y).collect_vec();
+    let mut fy = xy.iter().map(|(x, y)| x - y).collect_vec();
+
+    fx.sort();
+    fy.sort();
+
+    let ans = max(
+        fx.last().unwrap() - fx.first().unwrap(),
+        fy.last().unwrap() - fy.first().unwrap(),
+    );
+    println!("{}", ans);
 }
