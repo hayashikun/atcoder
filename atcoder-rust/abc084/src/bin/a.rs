1ced05d40f1970cef983f5f9d8c9645f7ff94343
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-04-19 23:26:39 +0900

    solve

--- atcoder-rust/abc084/src/bin/a.rs
+++ atcoder-rust/abc084/src/bin/a.rs
@@ -1,9 +1,12 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input!{
+        m: usize
+    };
+
+    println!("{}", 48 - m);
 }
--- atcoder-rust/abc084/src/bin/b.rs
+++ atcoder-rust/abc084/src/bin/b.rs
@@ -1,9 +1,38 @@
-// TODO
 #![allow(unused_imports)]
 
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
-    input!{};
+    input! {
+        a: usize,
+        b: usize,
+        s: Chars
+    };
+
+    if check(a, b, s) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+fn check(a: usize, b: usize, s: Vec<char>) -> bool {
+    for i in 0..a {
+        if !s[i].is_numeric() {
+            return false;
+        }
+    }
+
+    if s[a] != '-' {
+        return false;
+    }
+
+    for i in (a + 1)..(a + b + 1) {
+        if !s[i].is_numeric() {
+            return false;
+        }
+    }
+
+    return true;
 }
